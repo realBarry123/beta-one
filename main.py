913e38895db2c126ef0d02e19f92bbfddc37a1b1
realBarry123 <77864928+realBarry123@users.noreply.github.com>
2023-07-15 20:16:00 +0800

    Andy took over my computer
    he gave some tips

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import chess, copy, math
 
 print("hello world :(")
 pieceScores = {"p": 1, "n": 3, "b": 3, "r": 5, "q": 9}
+positions_evaluated = 0
 
 def score(board):
     finalScore = 0
@@ -19,28 +20,39 @@ def score(board):
     return finalScore
 
 def search(board, depth, isWhite):
-
-    if depth == 0: return (score(board), "")
+    global positions_evaluated
+    positions_evaluated += 1
+    if depth == 0: return (score(board), "")  # return "n" as well as a move?
     if board.is_checkmate():
-        if board.turn == isWhite: return (-math.inf, "n")
-        return (math.inf, "n")
+        if board.turn == isWhite: return (-math.inf, "n")  # "n" signifies end of game or no moves
+        return (math.inf, "n")  # add special case checking for "n" as returned move
     movesDict = {}
-    for move in board.legal_moves:
+    for move in board.legal_moves:  # keep in mind that move represents a Move object and not a string
         newBoard = copy.copy(board)
         newBoard.push(move)
         movesDict[search(newBoard, depth-1, isWhite)[0]] = move
-    if board.turn == isWhite:
+    if board.turn == isWhite:  # replace isWhite with chess.WHITE?
         return (max(movesDict), movesDict[max(movesDict)])
     return (min(movesDict), movesDict[min(movesDict)])
 
 chessBoard = chess.Board()
 
-for i in range(10):
+for i in range(20):
+    print(positions_evaluated)
+    positions_evaluated = 0
     nextMove = search(chessBoard,3,True)[1]
     chessBoard.push(nextMove)
     print(chessBoard)
+    print(positions_evaluated)
+    positions_evaluated = 0
     print(score(chessBoard))
     nextMove = search(chessBoard, 3, False)[1]
     chessBoard.push(nextMove)
+    '''
+    try:
+        chessBoard.push(nextMove)
+    except AttributeError:
+        break
+    '''
     print(chessBoard)
     print(score(chessBoard))
